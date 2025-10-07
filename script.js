4f324da780acdd6a894c601a229c80a11ca926cb
frankmuellerpierstorff <frankmuellerpierstorff@gmail.com>
2025-10-07 19:24:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -17,43 +17,35 @@
   items.forEach(el => io.observe(el));
 })();
 
-// Handle video loading - images for mobile, videos for desktop
+// Simple video loading
 (function() {
-  const isMobile = window.innerWidth <= 768;
   const heroVideo = document.querySelector('.hero__video');
   const heroPlaceholder = document.querySelector('.hero__placeholder');
   const inlineVideo = document.querySelector('.inline-video');
   const inlinePlaceholder = document.querySelector('.inline-placeholder');
   
-  if (isMobile) {
-    // Mobile: just show images, hide videos
-    if (heroVideo) heroVideo.style.display = 'none';
-    if (inlineVideo) inlineVideo.style.display = 'none';
-  } else {
-    // Desktop: try to play videos
-    const playVideos = () => {
-      if (heroVideo && heroPlaceholder) {
-        heroVideo.play().then(() => {
-          heroPlaceholder.style.opacity = '0';
-          heroVideo.classList.add('playing');
-        }).catch(() => {});
-      }
-      
-      if (inlineVideo && inlinePlaceholder) {
-        inlineVideo.play().then(() => {
-          inlinePlaceholder.style.display = 'none';
-          inlineVideo.style.display = 'block';
-          inlineVideo.classList.add('playing');
-        }).catch(() => {});
-      }
-    };
+  const playVideos = () => {
+    if (heroVideo && heroPlaceholder) {
+      heroVideo.play().then(() => {
+        heroPlaceholder.style.opacity = '0';
+        heroVideo.classList.add('playing');
+      }).catch(() => {});
+    }
     
-    // Try immediately
-    playVideos();
-    
-    // Try on any user interaction
-    ['touchstart', 'touchend', 'click', 'scroll'].forEach(event => {
-      document.addEventListener(event, playVideos, { once: true, passive: true });
-    });
-  }
+    if (inlineVideo && inlinePlaceholder) {
+      inlineVideo.play().then(() => {
+        inlinePlaceholder.style.display = 'none';
+        inlineVideo.style.display = 'block';
+        inlineVideo.classList.add('playing');
+      }).catch(() => {});
+    }
+  };
+  
+  // Try immediately
+  playVideos();
+  
+  // Try on any user interaction
+  ['touchstart', 'touchend', 'click', 'scroll'].forEach(event => {
+    document.addEventListener(event, playVideos, { once: true, passive: true });
+  });
 })();
